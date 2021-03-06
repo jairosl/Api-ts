7697188297733eaed813c8a25141037ff22948da
jairosl <jairo.lima@aluno.uepb.edu.br>
2021-03-06 10:25:18 -0300

    feat: create migration address

---
+++ src/database/migrations/1615036047788-CreateAddress.ts
@@ -0,0 +1,64 @@
+import { MigrationInterface, QueryRunner, Table } from "typeorm";
+
+export class CreateAddress1615036047788 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: "address",
+        columns: [
+          {
+            name: "id",
+            type: "uuid",
+          },
+          {
+            name: "street",
+            type: "varchar",
+          },
+          {
+            name: "district",
+            type: "varchar",
+          },
+          {
+            name: "number",
+            type: "numeric",
+          },
+          {
+            name: "complement",
+            type: "varchar",
+            isNullable: true,
+          },
+          {
+            name: "cep",
+            type: "varchar",
+          },
+          {
+            name: "city",
+            type: "varchar",
+          },
+          {
+            name: "state",
+            type: "varchar",
+          },
+          {
+            name: "user_id",
+            type: "uuid",
+          },
+        ],
+        foreignKeys: [
+          {
+            name: "FKUser",
+            referencedTableName: "users",
+            referencedColumnNames: ["id"],
+            columnNames: ["user_id"],
+            onDelete: "CASCADE",
+            onUpdate: "CASCADE",
+          },
+        ],
+      })
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable("address");
+  }
+}
