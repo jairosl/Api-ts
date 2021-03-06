4317721db1042d6bc92402ac61993f0423a3bfaf
jairosl <jairo.lima@aluno.uepb.edu.br>
2021-03-06 14:05:51 -0300

    feat: compare hash with password

--- src/utils/hash.ts
+++ src/utils/hash.ts
@@ -4,3 +4,8 @@ export default async function hash(password: string) {
   const hashPassword = bcryptjs.hashSync(password);
   return hashPassword;
 }
+
+export async function compareHash(hashPassword: string, password: string) {
+  const comparePassword = bcryptjs.compare(password, hashPassword);
+  return comparePassword;
+}
