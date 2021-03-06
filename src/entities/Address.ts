21d0281b64cdab23e71331c8603c5639f303f362
jairosl <jairo.lima@aluno.uepb.edu.br>
2021-03-06 10:36:06 -0300

    feat: Create entite Address

---
+++ src/entities/Address.ts
@@ -0,0 +1,48 @@
+/* eslint-disable camelcase */
+import {
+  Column, Entity, JoinColumn, ManyToOne, PrimaryColumn,
+} from "typeorm";
+import { v4 as uuid } from "uuid";
+import { User } from "./User";
+
+@Entity("address")
+class Address {
+  @PrimaryColumn()
+  readonly id!: string;
+
+  @Column()
+  street!: string;
+
+  @Column()
+  district!: string;
+
+  @Column()
+  number!: number;
+
+  @Column()
+  complement!: string;
+
+  @Column()
+  cep!: string;
+
+  @Column()
+  city!: string;
+
+  @Column()
+  state!: string;
+
+  @Column()
+  user_id!: string;
+
+  @ManyToOne(() => User)
+  @JoinColumn({ name: "user_id" })
+  user!: User;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuid();
+    }
+  }
+}
+
+export { Address };
